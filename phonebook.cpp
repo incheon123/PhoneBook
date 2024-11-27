3a4802ee4d5cb1a68a0cad48ad85b4c0f39fca72
incheon123 <xorgus704@gmail.com>
2024-11-27 15:14:00 +0900

    list available db driver

--- phonebook.cpp
+++ phonebook.cpp
@@ -1,13 +1,15 @@
 #include "phonebook.h"
 #include "./ui_phonebook.h"
 
-
+#include <QDebug>
+#include <QtSql/QSqlDatabase>
 
 PhoneBook::PhoneBook(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::PhoneBook)
 {
     ui->setupUi(this);
+    qDebug() << QSqlDatabase::drivers();
 }
 
 PhoneBook::~PhoneBook()
@@ -18,14 +20,13 @@ PhoneBook::~PhoneBook()
 void PhoneBook::switchPage()
 {
     QStackedWidget *stack = ui->stackedWidget;
-    if(stack->currentIndex() == 0){
+    if (stack->currentIndex() == 0) {
         stack->setCurrentIndex(1);
-    }else if(stack->currentIndex() == 1){
+    } else if (stack->currentIndex() == 1) {
         stack->setCurrentIndex(2);
-    }else{
+    } else {
         stack->setCurrentIndex(0);
     }
-
 }
 
 void PhoneBook::on_pushButton_clicked()
@@ -33,18 +34,13 @@ void PhoneBook::on_pushButton_clicked()
     QString user_id = ui->user_id->text();
     QString user_pw = ui->user_pw->text();
 
-    if(user_id == id){
+    if (user_id == id) {
         // id is correct
 
-        if(user_pw == pw){
+        if (user_pw == pw) {
             //pw is correct
-
-
         }
     }
 
-
-
     switchPage();
 }
-
