3a66b3bd1b6c35913679ad2925182a7b5d0b9452
incheon123 <xorgus704@gmail.com>
2024-12-01 22:24:16 +0900

    회원가입, 로그인, 비밀번호찾기 함수,  속성 추가

--- phonebook.h
+++ phonebook.h
@@ -8,7 +8,8 @@
 #include <QSqlDatabase>
 #include <QMessageBox>
 
-#include "db.h"
+#include "src_h/db_h/db.h"
+#include "home.h"
 
 #define SIGNIN 1
 #define FIND_ACCOUNT 2
@@ -27,7 +28,16 @@ class PhoneBook : public QMainWindow
 public:
     PhoneBook(QWidget *parent = nullptr);
     ~PhoneBook();
+
+public:
     Db *db;
+    home *h;
+
+public:
+    PhoneBook getPhoneBook();
+
+signals:
+    void click_back(int p);
 
 private slots:
     void on_signin_clicked();
@@ -40,21 +50,27 @@ private slots:
 
     void on_signup_submit_btn_clicked();
 
+    void on_submit_clicked();
+
+    void on_find_account_back_btn_clicked();    // back
+
+    void on_signup_back_btn_clicked();  // back
+
 private:
     Ui::PhoneBook *ui;
+
+    QString id = "opportunity13";
+    QString pw = "rlghlek153@";
+    QString signup_userId;
+    QString signup_userPw;
+    QString signup_phone_number;
+    bool duplicateChk = 0;
+
+private:
     void switchPage(int p);
     bool validUserId();
     bool validUserPw();
     bool validUserPhoneNum();
     void execMsgBox(QString content);
-
-private:
-    QString id = "opportunity13";
-    QString pw = "rlghlek153@";
-
-    QString signup_userId;
-    QString signup_userPw;
-    QString signup_phone_number;
-    bool duplicateChk = 0;
 };
 #endif // PHONEBOOK_H
