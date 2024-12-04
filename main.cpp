b3f041509b4f9f9ff31f69ffc3bf9b9001de12cc
incheon123 <xorgus704@gmail.com>
2024-12-04 22:24:09 +0900

    add QSqlDatabase for db connection

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "phonebook.h"
+#include "../../../../src_h/db_h/db.h"
 
 #include <QApplication>
 
@@ -7,5 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     PhoneBook w;
     w.show();
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+
+    qDebug() << db.connectionName();
     return a.exec();
 }
