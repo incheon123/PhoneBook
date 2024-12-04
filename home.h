826ff4041dd21ff729c98f29f0597fe9d1fa5228
incheon123 <xorgus704@gmail.com>
2024-12-04 22:21:58 +0900

    add loaduser method, user

--- home.h
+++ home.h
@@ -2,6 +2,11 @@
 #define HOME_H
 
 #include <QWidget>
+#include <QTableWidget>
+#include <QListWidget>
+#include <QLabel>
+
+#include "../../../../src_h/db_h/db.h"
 
 namespace Ui {
 class home;
@@ -19,10 +24,32 @@ public:
     void save(QWidget* widget);
     void openHome();
     void closeHome();
-private:
-    Ui::home *ui;
+private slots:
+
+    void on_index_addBtn_clicked();
+
+    void on_index_deleteBtn_clicked();
+
+    void on_index_updateBtn_clicked();
+
+    void on_index_saveBtn_clicked();
+
+signals:
+    void addBtn_clicked();
 
 private:
+    Ui::home *ui;
+    QTableWidget* table;
+    QListWidget* list;
+    QLabel* welcomeLabel;
+    QLabel* lastLoginDate;
+    Db* db;
+    UserAccount* user;
+
+private:
+    void errorMsg(QString);
+    void msg(QString);
+    void loadUser();
 };
 
 #endif // HOME_H
