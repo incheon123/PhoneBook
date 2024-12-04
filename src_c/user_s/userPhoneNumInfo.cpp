26b70d646ab1745ff9420b03f6a789699d467938
incheon123 <xorgus704@gmail.com>
2024-12-04 22:31:07 +0900

    userAccount 객체 안에 집합형태로 저장됨, 주로 테이블 데이터를  담는 용도로 사용

---
+++ src_c/user_s/userPhoneNumInfo.cpp
@@ -0,0 +1,32 @@
+#include "../../../../src_h/user_h/userPhoneNumInfo.h"
+
+QString UserPhoneNumInfo::getOwner(){
+    return this->owner;
+}
+QString UserPhoneNumInfo::getPhoneNumUserId(){
+    return this->phoneNumUserId;
+}
+QString UserPhoneNumInfo::getPhoneNumber(){
+    return this->phoneNumber;
+}
+QString UserPhoneNumInfo::getName(){
+    return this->name;
+}
+QString UserPhoneNumInfo::getCreateTime(){
+    return this->createTime;
+}
+void UserPhoneNumInfo::setOwner(QString owner){
+    this->owner = owner;
+}
+void UserPhoneNumInfo::setPhoneNumUserId(QString phoneNumUserId){
+    this->phoneNumUserId = phoneNumUserId;
+}
+void UserPhoneNumInfo::setPhoneNumber(QString phoneNumber){
+    this->phoneNumber = phoneNumber;
+}
+void UserPhoneNumInfo::setName(QString name){
+    this->name = name;
+}
+void UserPhoneNumInfo::setCreateTime(QString createTime){
+    this->createTime = createTime;
+}
