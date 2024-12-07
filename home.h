ae3bed8d503c6b74c6686bd3cf933a2853dfeb34
incheon123 <xorgus704@gmail.com>
2024-12-07 23:35:35 +0900

    add two signal, profile

--- home.h
+++ home.h
@@ -7,6 +7,7 @@
 #include <QLabel>
 
 #include "../../../../src_h/db_h/db.h"
+#include "../../../../src_h/profile_h/profile.h"
 
 namespace Ui {
 class home;
@@ -24,32 +25,55 @@ public:
     void save(QWidget* widget);
     void openHome();
     void closeHome();
+    home* getUi();
+    Ui::home *ui;
+
 private slots:
-
-    void on_index_addBtn_clicked();
-
     void on_index_deleteBtn_clicked();
-
-    void on_index_updateBtn_clicked();
 
     void on_index_saveBtn_clicked();
 
+    void on_index_table_cellClicked(int row, int column);
+    void showContextMenu(const QPoint &pos);
+    void mousePressEvent(int, int);
+    void on_index_sideMenu_itemClicked(QListWidgetItem *item);
+
 signals:
-    void addBtn_clicked();
+    void on_index_table_customContextMenuRequested(const QPoint &pos);
+    void decreaseNumOfPhoneNumber(QLineEdit*, int num);
+    void increaseNumOfPhoneNumber(QLineEdit*, int num);
 
 private:
-    Ui::home *ui;
+    Profile* profile;
     QTableWidget* table;
     QListWidget* list;
     QLabel* welcomeLabel;
     QLabel* lastLoginDate;
     Db* db;
     UserAccount* user;
+    QString oldPhoneNumber;
+    QString oldName;
+    QString oldUserId;
+    QString newPhoneNumber;
+    QString newName;
+    QString newUserId;
+    int selectedRow;
 
 private:
     void errorMsg(QString);
     void msg(QString);
     void loadUser();
+    bool findId(QString, QString);
+    void save(QString, QString, QString, QString);
+    void modify(QString, QString, QString);
+    bool remove(QString, QString);
+    void insertIntoTable(QString, QString, QString, QString);
+    void modifyTable(QString, QString);
+    void removeRowInTable(int row);
+    void setProfile();
+    QListWidget* getList();
+    /* 임시 method */
+    // void printSelectedRows();
 };
 
 #endif // HOME_H
