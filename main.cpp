e28d7ae35322c0df4340efb669b70c231335234c
incheon123 <xorgus704@gmail.com>
2025-01-22 19:43:26 +0900

    add ChatBox.show()

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include "phonebook.h"
 #include "properties.h"
-
 #include <QApplication>
+
+#include "../../../../ui/chat/chatbox.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
     db.setHostName(HOST_NAME);
     db.setDatabaseName(DATABASE_NAME);
     db.setUserName(USER_NAME);
     db.setPassword(PASSWORD);
+
     PhoneBook w;
-    w.show();
+    // w.show();
+
+    ChatBox cb;
+    cb.show();
     return a.exec();
 }
